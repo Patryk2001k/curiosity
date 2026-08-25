aaec6ba2222d098fd6fa4884c04c74ca12b97502
Patryk Wróblewski <patryk.wroblewski@poczta.onet.pl>
2026-08-25 18:22:12 +0200

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -45,7 +45,7 @@ head: [
           items: [
             { text: 'Support & SLA', link: '/support/' },
             { text: 'Privacy Policy', link: '/legal/privacy-policy' },
-            { text: 'Terms of Service', link: '/legal/terms-of-service' }
+            { text: 'Terms of Service', link: '/legal/terms-of-service' },
             { text: 'Security Policy', link: '/legal/security-policy' },
           ]
         }
